f5942c4fa8f76e6b0fc115c120a45aab55bc0be4
Tim Andes <tim.andes@deltavdigital.com>
2021-09-22 15:06:33 -0600

    prep frontpage.php

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package camera-work-theme-2021
+ */
+
+get_header();
+?>
+<!-- tailwind grid or pagebuilder for body -->
+<?php
+get_footer();
+?>
