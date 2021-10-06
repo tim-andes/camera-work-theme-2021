1b18874a23433349b6fa16e9b562f46343f1b69d
Tim Andes <tim.andes@deltavdigital.com>
2021-10-06 16:27:36 -0600

    import custom font

--- header-front.php
+++ header-front.php
@@ -14,10 +14,15 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<!-- TO DO: Create one head.php file to include in all headers. -->
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<!-- Add Custom Font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    
     <?php wp_head(); ?>
 </head>
 
@@ -28,7 +33,7 @@
     <header id="header">
         
         <!--   Announcements   -->
-        <div id="announcements-container-front-hero">
+        <div id="announcements-container-front-hero" class="">
             <div id="announcements-text-front-hero">
                 Holiday Sale! All Gallery Prints 20% off until Jan. 1, 2022!
             </div><!-- #announcements-text-front-hero -->
