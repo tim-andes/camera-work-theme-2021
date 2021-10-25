cdaf76ce843428ef9cad71306f30cefabb9699d1
Tim Andes <tim.andes@deltavdigital.com>
2021-10-25 14:07:47 -0600

    delete unneeded _s markup

--- header.php
+++ header.php
@@ -29,37 +29,24 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'camera-work-theme-2021' ); ?></a>
+	<div id="body-container" class="flex flex-row">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'camera-work-theme-2021' ); ?></a>
 
-	<header id="masthead" class="site-header flex flex-row">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header mr-5">
+			<div class="site-branding">
 				<?php
-			else :
+				the_custom_logo();
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$camera_work_theme_2021_description = get_bloginfo( 'description', 'display' );
-			if ( $camera_work_theme_2021_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $camera_work_theme_2021_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			</div><!-- END .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'camera-work-theme-2021' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<nav id="site-navigation" class="main-navigation ">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- END #site-navigation -->
+		</header><!-- END #masthead -->
