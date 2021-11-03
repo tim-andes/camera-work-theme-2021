fe337d0496eb60a20eedae6ef2ee3bf1b3c5b293
Tim Andes <tim.andes@deltavdigital.com>
2021-11-03 15:16:15 -0600

    remove extraneous markup from 404 page

--- 404.php
+++ 404.php
@@ -10,49 +10,21 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="404-main" class="site-main  min-h-screen lg:grid-cols-6 lg:grid-rows-6">
 
-		<section class="error-404 not-found">
+		<div class="404-main-child error-404 not-found min-h-full lg:col-start-2 lg:col-span-4 lg:row-start-2 lg:row-span-4">
 			<header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'camera-work-theme-2021' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'camera-work-theme-2021' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'camera-work-theme-2021' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$camera_work_theme_2021_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'camera-work-theme-2021' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$camera_work_theme_2021_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				<!-- Create a stylized 404 -->
+				<div class="back-button-container">
+					<?php do_action('back_button'); ?>
+				</div>			
 			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+
+		</div><!-- .404-main-child .error-404 .not-found-->
 
 	</main><!-- #main -->
 
