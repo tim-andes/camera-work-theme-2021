fb3666fce320c47604f8c93561942a0089045259
Tim Andes <tim.andes@deltavdigital.com>
2021-12-01 11:09:40 -0700

    remove extraneous divs and add grid back

--- page-contact.php
+++ page-contact.php
@@ -15,14 +15,16 @@
 get_header(); // not front page header, but normal header
 ?>
 
-	<main id="contact-main" class="site-main min-h-full">
+	<main id="contact-main" class="site-main lg:col-start-3 lg:col-end-11 md:col-start-3 md:col-end-6 row-start-2">
 		<!-- SiteOrigin Page Builder for content -->
-        <div id="contact-form">
+        <div class="grid-container-1"></div>
+        <div id="contact-form" class="grid-container-2">
             <?php 
                 $page = get_page_by_title('Contact');
                 echo apply_filters('the_content', $page->post_content);
             ?>
         </div>
+        <div class="grid-container-3"></div>
 	</main><!-- #contact-main .site-main -->
 </div><!-- body-container Note: starts in header.php -->
 
