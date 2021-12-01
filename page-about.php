d6c715848058f9d2879c98eacc4f504faf0fce92
Tim Andes <tim.andes@deltavdigital.com>
2021-12-01 11:10:10 -0700

    add grid remove extra divs

--- page-about.php
+++ page-about.php
@@ -10,20 +10,23 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package camera-work-theme-2021
+ *  md:col-start-2 md:col-end-6
  */
 
 get_header(); // not front page header, but normal header
 ?>
 
-	<main id="about-main" class="site-main flex-row content-center min-h-full">
+	<main id="about-main" class="site-main lg:col-start-3 lg:col-end-11 row-start-2">
 		<!-- SiteOrigin Page Builder for content -->
-        <div id="about-container">
+        <!-- <div class="grid-container-1"></div> -->
+        <div id="about-container" class="grid-container-2 lg:row-start-3">
             <?php 
                 $page = get_page_by_title('About');
                 echo apply_filters('the_content', $page->post_content);
             ?>
-        </div>
-	</main><!-- #about .site-main -->
+        </div><!-- #about-container -->
+        <!-- <div class="grid-container-3"></div> -->
+	</main><!-- #about-main .site-main -->
 </div><!-- body-container Note: starts in header.php -->
 
 <?php
